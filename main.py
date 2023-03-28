8b45e2e1cf0e9750a2fd42e1101e2c1180e79027
bowlersp <120626722+bowlersp@users.noreply.github.com>
2023-03-28 17:28:58 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import smtplib
+import datetime as dt
+import random
+
+MY_EMAIL = "bowlers@yahoo.com"
+MY_PASSWORD = "sgolwfrpbwtnkunc"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+#create if/then statement to verify it is Monday, if it is then perform the code
+if weekday == 1:
+    with open("quotes.txt") as quote_file:
+      all_quotes = quote_file.readlines()
+      quote = random.choice(all_quotes)
+
+    print(quote)
+    with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+      connection.starttls()
+      connection.login(MY_EMAIL, MY_PASSWORD)
+      connection.sendmail(
+          from_addr=MY_EMAIL, 
+          to_addr=MY_EMAIL, 
+          msg=f"Subject: Monday Motivation\n\n {quote}"
+      )
