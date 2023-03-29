a1da76dab6f890868043d44ddfec37fb616f265c
bowler <m.bowler@f5.com>
2023-03-28 22:49:03 -0500

    initial commit

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import smtplib
 import datetime as dt
 import random
 
-MY_EMAIL = "bowlers@yahoo.com"
-MY_PASSWORD = "xxxxxxxxx"
+MY_EMAIL = "@gmail.com"
+MY_PASSWORD = ""
 
 now = dt.datetime.now()
 weekday = now.weekday()
@@ -12,13 +12,20 @@ if weekday == 1:
     with open("quotes.txt") as quote_file:
       all_quotes = quote_file.readlines()
       quote = random.choice(all_quotes)
+      quote = quote.replace('“', '"')
+      quote = quote.replace('”', '"')
+      quote = quote.replace('—', '-')
+      quote = quote.replace('―', '-')
+      quote = quote.replace('’', '\'')
 
     print(quote)
-    with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
       connection.starttls()
       connection.login(MY_EMAIL, MY_PASSWORD)
+      # buffer = quote.replace("\"", "", -1)
+      # print(buffer)
       connection.sendmail(
           from_addr=MY_EMAIL, 
-          to_addr=MY_EMAIL, 
-          msg=f"Subject: Monday Motivation\n\n {quote}"
+          to_addrs=MY_EMAIL, 
+          msg=f'Subject: Monday Motivation\n\n {quote}'
       )
